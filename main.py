f32fac1e6e42e99e2b0018e5c7b038c1fd39f89c
Эдуард Кусков <shadowfish@yandex.ru>
2024-03-11 22:00:44 +0300

    realisation of requests

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.api_hh import HhApi
 
 def user_interaction(userinput: bool = True):
     platforms = ["HeadHunter"]
-    search_query = "Введите поисковый запрос: "
+    search_region = 'Санкт-Петербург'
     top_n = 13
     # filter_words = "Python backend программист fullstack".split()
     filter_words = "Python"
@@ -12,10 +12,18 @@ def user_interaction(userinput: bool = True):
 
     if userinput:
         platforms = ["HeadHunter"]
-        search_query = input("Введите поисковый запрос: ")
+        search_region = input("Введите регион или город для поиска вакансий")
         top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-        filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+        filter_words = input("Введите ключевые слова для фильтрации вакансий (через пробел): ").split()
         salary_range = input("Введите диапазон зарплат: ")  # Пример: 100000 - 150000
+
+
+    # text = 'Мурино'
+    # # text = 'Мухосранск'
+    #
+    # id1 = hh_api_hh.get_request_area(area_name=text)
+    # print(f"{text}, id= {id1}")
+
 
     # filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
     #
@@ -24,20 +32,55 @@ def user_interaction(userinput: bool = True):
     # sorted_vacancies = sort_vacancies(ranged_vacancies)
     # top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
     # print_vacancies(top_vacancies)
-    hh_api_hh = HhApi('https://api.hh.ru/')
+
+
+    hh_api_hh = HhApi()
+    #
+    # text = 'Ленинградская область'
+    # # text = 'Мухосранск'
+    #
+    # id1 = hh_api_hh.get_request_area(area_name=text)
+    # print(f"{text}, id= {id1}")
+
 
     # area_id = 113 - регион Россия
     # Мурино: id= 5084
     # Санкт-Петербург id = 2
-    res = hh_api_hh.get_request('vacancies', text=filter_words, area='2', per_page=50)
+
+    region_id = hh_api_hh.get_request_area(area_name=search_region)
+
+    if region_id is None:
+        res = hh_api_hh.get_request(text=filter_words, per_page=50)
+        print("region_id is None!")
+    else:
+        res = hh_api_hh.get_request(text=filter_words, area=str(region_id), per_page=5)
 
     # URL = 'https://api.hh.ru/vacancies'
 
     # res = requests.get(URL, params={'text': 'Python'})
 
-    print(res.status_code)
+    # print(res.status_code)
     # print(res.json())
-    print_dict_recursive(dict(res.json()), 0)
+    for item in res:
+        print_dict_recursive(item, 0)
+
+    print(f"\n\n\n NEXT PAGE \n\n\n")
+    res = hh_api_hh.get_request_next_page()
+    for item in res:
+        print_dict_recursive(item, 0)
+
+
+    # area_id = 113 - регион Россия
+    # Мурино: id= 5084
+    # Санкт-Петербург id = 2
+
+    # text = 'Санкт-Петербург'
+    # text = 'Россия'
+    # text = 'Мурино'
+    # # text = 'Мухосранск'
+    #
+    # id1 = hh_api_hh.get_request_area(area_name=text)
+    # print(f"{text}, id= {id1}")
 
     # res = hh_api_hh.get_request_area('areas/?area_id=Null')
     # res = hh_api_hh.get_request_area('areas/?id=1530')
@@ -45,16 +88,16 @@ def user_interaction(userinput: bool = True):
     # print_areas(res.json())
 
 
-def print_areas(areas):
-    for area in areas:
-        if area['name'] == 'Россия':
-            for area_rf in area['areas']:
-                print(f"\t{area_rf['name']}: id= {area_rf['id']}")
-                if area_rf['name'] == 'Ленинградская область' or area_rf['name'] == 'Санкт-Петербург':
-                    for area_sp in area_rf['areas']:
-                        print(f"\t\t{area_sp['name']}: id= {area_sp['id']}")
-        else:
-            print(area['name'])
+# def print_areas(areas):
+#     for area in areas:
+#         if area['name'] == 'Россия':
+#             for area_rf in area['areas']:
+#                 print(f"\t{area_rf['name']}: id= {area_rf['id']}")
+#                 if area_rf['name'] == 'Ленинградская область' or area_rf['name'] == 'Санкт-Петербург':
+#                     for area_sp in area_rf['areas']:
+#                         print(f"\t\t{area_sp['name']}: id= {area_sp['id']}")
+#         else:
+#             print(area['name'])
 
 def print_dict_recursive(dictionary, i):
     i += 1
@@ -62,7 +105,7 @@ def print_dict_recursive(dictionary, i):
         if isinstance(value, dict):
             print(f"{' ' * (i-1)}{key} =")
             print_dict_recursive(value, i)
-        elif isinstance(value, list):
+        elif isinstance(value, list) and len(value) > 0:
             print(f"{' ' * (i - 1)}{key} = \n[")
             for l in value:
                 print_dict_recursive(l, i)
--- src/api_hh.py
+++ src/api_hh.py
@@ -21,39 +21,82 @@ class HhApi(AbstractApiNoAuth):
     Класс для работы с API сервиса hh.ru
     """
 
-    def __init__(self, url: str):
+    def __init__(self, url: str = 'https://api.hh.ru/'):
         self.__url = url
-        # self.__parameters = parameters
+        self.__last_parameters = []
+        self.__last_sub_url = ''
+        self.found = 0
+        self.pages = 0
+        self.page = 0
+        self.per_page = 0
 
-    def get_request(self, sub_url: str = '', **parameters: dict[str: str]) -> requests:
+    def get_request(self, sub_url: str = 'vacancies', **parameters: dict[str: str]) -> list[dict]:
         # URL = 'https://api.hh.ru/vacancies'
 
         res = requests.get(self.__url + sub_url, params=parameters)
-        return res
+        if res.status_code != 200:
+            raise Exception(f"Request code= {res.status_code}, request='{self.__url}{sub_url}', params={parameters}")
 
-    def get_request_area(self, sub_url: str, **parameters: dict[str: str]) -> requests:
-        """
-        Сделаем возврат ID по имени населенного пункта или области
-        Дополнительно вывод список областей возможно, но зачем?
-        :param sub_url:
-        :param parameters:
-        :return:
-        """
+        self.__last_parameters = parameters
+        self.__last_sub_url = sub_url
+        self.found = res.json()['found']
+        self.pages = res.json()['pages']
+        self.page = res.json()['page']
+        self.per_page = res.json()['per_page']
+
+        return res.json()['items']
+
+    def get_request_next_page(self) -> list[dict]:
         # URL = 'https://api.hh.ru/vacancies'
 
-        res = requests.get(self.__url + sub_url, params=parameters)
-        # res = hh_api_hh.get_request_area('areas', area_id=113)
+        if self.pages < self.page:
+            self.page += 1
+            self.__last_parameters['page'] = self.page
+        else:
+            ValueError("No more pages to request!")
 
-        # print(res.status_code)
-        #
-        # print(res.json())
-        # >> > {'alternate_url': 'https://hh.ru/search/vacancy?enable_snippets=true&text=Python',
-        #       'arguments': None,
-        #       'clusters': None,
-        #       'fixes': None,
-        #       'found': 13182,
-        #       'items': [...]
-        #       }
-        # print(f"response2: status_code: {response2.status_code} \n result: \n {response2.text}")
+        res = requests.get(self.__url + self.__last_sub_url, params=self.__last_parameters)
+        if res.status_code != 200:
+            raise Exception(f"Request code= {res.status_code}, request='{self.__url}{sub_url}', params={parameters}")
 
-        return res
+        self.found = res.json()['found']
+        self.pages = res.json()['pages']
+        self.page = res.json()['page']
+        self.per_page = res.json()['per_page']
+
+        return res.json()['items']
+
+
+    def get_request_area(self, sub_url: str = 'areas', area_name: str = 'Москва') -> str | None:
+        """
+        Dозврат ID по имени населенного пункта или области
+        """
+        res = requests.get(self.__url + sub_url)
+
+        if res.status_code != 200:
+            raise Exception(f"Request code= {res.status_code}, request='{self.__url}{sub_url}'")
+
+        # print(f"Request code= {res.status_code}, request='self.__url + sub_url'")
+
+        area = res.json()
+
+        result = self.recursive_find_area_id(area, area_name)
+
+        return result
+
+    @staticmethod
+    def recursive_find_area_id(areas, area_name) -> int | None:
+        """
+        Найти в древовидной структуре словаря areas (HH.ru) искомый город
+        """
+        for area in areas:
+            if area['name'] == area_name:
+                return area['id']
+            elif isinstance(area['areas'], list) and len(area['areas']) > 0:
+                result = HhApi.recursive_find_area_id(area['areas'], area_name)
+                if result is not None:
+                    return result
+            else:
+                continue
+        else:
+            return None
