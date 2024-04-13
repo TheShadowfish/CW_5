c3f1ff9c2cfe6212fc9ffeb155ebd4b13d4fd487
Эдуард Кусков <shadowfish@yandex.ru>
2024-04-13 14:37:06 +0300

    реализация запросов к БД в dbconnector завершена

--- src/dbconnector.py
+++ src/dbconnector.py
@@ -1,8 +1,6 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT  # <-- ADD THIS LINE
 
-# import os
-# import csv
 from src.connectors import LoadWrite, AbsoluteFileConnector
 from decouple import config
 from abc import ABC, abstractmethod
@@ -38,34 +36,25 @@ class DBManagerNeedToPerform(ABC):
         """
 
     @abstractmethod
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, word_list: list[str]):
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
         """
         pass
 
 
-class DBManager(AbsoluteFileConnector, LoadWrite):
+class DBManager(AbsoluteFileConnector, LoadWrite, DBManagerNeedToPerform):
     """
-    Имеются уникальные объекты
+    Связь с базой данных, создание экземпляров класса Vacancy и Employer
+    Запись их в базу данных согласно спроектированной структуре таблиц
+    Создание БД и таблиц, если нет самой БД
+    Вывод запросов к БД в терминал согласно запрошенным методам класса DBManager
 
-            Vacancy - вакансии
-    name, url  -  string
-    salary - int
-
-    region - string - список регионов, данные повторяются, характеризуется region_id (int)
-    requirements - string (длинный текст)
-
-    employer_id - int, связь с работодателем, многие к одному
-    region_id - связь со списком регионов, многие ко многим
-
-            Employer - работодатель
-    id - int, связь с employer_id (Vacancy) - один ко многим
-    name, url, vacancies_url - string
+    Наследует ТОЛЬКО ABS классы, так что реализовывать пришлось все
     """
 
     def __init__(self):
-        self.file_extension = 'PostgreSQL'
+        self.file_extension = 'PostgresSQL'
         self.__db_name = 'headhunter_cw5'
         self.__tables = ('regions', 'employers', 'vacancies')
         # параметры user и password нужно конечно хранить отдельно
@@ -104,14 +93,14 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
             print(f'База данных {self.conn_params} уже существует. Продолжаем работу.')
 
     def create_headhunter_cw5_db(self):
-        # БД postrges должна быть в любом случае
-        postgress_params = {
+        # БД PostgreSQL должна быть в любом случае
+        postgre_params = {
             "host": "localhost",
             "database": "postgres",
             "user": config('DB_POSTRESQL_USER'),
             "password": config('DB_POSTRESQL_PASSWORD')}
 
-        con = psycopg2.connect(**postgress_params)
+        con = psycopg2.connect(**postgre_params)
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # <-- ADD THIS LINE
         cur = con.cursor()
 
@@ -120,12 +109,26 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
         con.close()
         print(f"{self.__db_name} создана! ")
 
+    def clear_all_tables(self):
+        print("Дропнем всё... А если перестараемся, то за себя не отвечаем. Диск С, кстати, тоже можно почистить?")
+        with psycopg2.connect(**self.conn_params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('TRUNCATE TABLE regions RESTART IDENTITY CASCADE;')
+                print("Таблица regions удалена!")
+                cur.execute('TRUNCATE TABLE employers RESTART IDENTITY CASCADE;')
+                print("Таблица employers удалена!")
+                cur.execute('TRUNCATE TABLE vacancies RESTART IDENTITY CASCADE;')
+                print("Таблица vacancies удалена!")
+                # TRUNCATE [ TABLE ] [ ONLY ] name [ * ] [, ... ] [ RESTART IDENTITY | CONTINUE IDENTITY ] [ CASCADE | RESTRICT ]
+
+        conn.close()
+
     def generate_bd_script(self):
         """
         Создание БД и таблиц:
         Спроектировать таблицы в БД PostgreSQL для хранения полученных данных о работодателях и их вакансиях.
         Для работы с БД используйте библиотеку psycopg2.
-        Вот здесь все таблицы и создаются, можно заценить.
+        Вот здесь все таблицы и создаются, можно посмотреть.
         """
         create_region = "CREATE TABLE regions (region_id int UNIQUE PRIMARY KEY, region_name varchar(50) NOT NULL);"
 
@@ -136,7 +139,6 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
                                     vacancies_url varchar(250) NOT NULL\
                                     );"
 
-        # надо бы id вакансии получать с hh, лучше, чем самому автогенерировать.
         create_vacancy = "CREATE TABLE vacancies (\
                                     vacancy_id int UNIQUE PRIMARY KEY,\
                                     name varchar(200) NOT NULL,\
@@ -149,13 +151,13 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
         print("Подключение...")
         with psycopg2.connect(**self.conn_params) as conn:
             with conn.cursor() as cur:
-                # cur.execute("INSERT INTO mytable VALUES (%s, %s, %s)", (4, name, description))
                 cur.execute(create_region)
-                print("Таблица region создана!")
+                print("Таблица regions создана!")
                 cur.execute(create_employers)
                 print("Таблица employers создана!")
                 cur.execute(create_vacancy)
-                print("Таблица vacancy создана!")
+                print("Таблица vacancies создана!")
+        conn.close()
 
     def print_database_table(self, table_number=0):
         if len(self.__tables) < table_number < len(self.__tables):
@@ -173,18 +175,14 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
         """
         Пишет в базу данных PostgreSQL информацию о работодателях и вакансиях.
 
-
-            Vacancy - вакансии
-        name, url  -  string
-        salary - int
-
+        Vacancy - вакансии:
+        name, url  -  string, salary - int,
         region - string - список регионов, данные повторяются, характеризуется region_id (int)
         requirements - string (длинный текст)
-
         employer_id - int, связь с работодателем, многие к одному
         region_id - связь со списком регионов, многие ко многим
 
-            Employer - работодатель
+        Employer - работодатель
         id - int, связь с employer_id (Vacancy) - один ко многим
         name, url, vacancies_url - string
         """
@@ -221,10 +219,13 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
             vacancies = self.checked_tables(vacancies, self.__tables[2])
 
         else:
-            print(f"Мы такого делать не будем, у нас пока стиралка не работает.")
-            return
+            # print(f"Мы такого делать не будем, у нас пока стиралка не работает.")
+            # print(f"Хотя нет, РАБОТАЕТ (*грохот на заднем фоне*)")
+            if input(f"Сейчас произойдет потенциально необратимая операция... Продолжать? Y/N") != 'Y':
+                self.clear_all_tables()
+                # input("Не прячьте ваши файлики по норам и углам, проверьте ваши файлики, иначе быть беде... Похоже ваши файлики затерты были там. Похоже ваши файлики затерты были... Где??")
 
-        # в данном случае конечно пишет таблицу в БД. Хотя она тоже в итоге файл, или несколько?
+        # В данном случае конечно пишет таблицу в БД. Хотя она тоже в итоге файл, или несколько?
         self.write_list_to_file(regions, 'regions')
         self.write_list_to_file(employers, 'employers')
         self.write_list_to_file(vacancies, 'vacancies')
@@ -235,21 +236,66 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
         file - название таблицы, куда вставляем
         """
         tuple_string = my_list
-        tablename = file
+        table_name = file
         if len(tuple_string) == 0:
-            print(f"{tablename} не содержит новых данных, не имеющихся в базе данных. Пропускаем.")
+            print(f"{table_name} не содержит новых данных, не имеющихся в базе данных. Пропускаем.")
             return
 
-        string_s = ', '.join(['%s' for i in range(len(tuple_string[0]))])
+        # string_s = ', '.join(['%s' for i in range(len(tuple_string[0]))])
+        string_s = ', '.join(['%s'] * len(tuple_string[0]))
         conn2 = psycopg2.connect(**self.conn_params)
         cur = conn2.cursor()
-        # print(f"INSERT INTO {tablename} VALUES ({string_s}) {tuple_string}")
 
         try:
-            cur.executemany(f"INSERT INTO {tablename} VALUES ({string_s})", tuple_string)
+            cur.executemany(f"INSERT INTO {table_name} VALUES ({string_s})", tuple_string)
         except Exception as e:
             print(f'\n ОШИБКА: {e} при записи следующего:')
-            print(f"INSERT INTO {tablename} VALUES ({string_s}) {tuple_string}")
+            print(f"INSERT INTO {table_name} VALUES ({string_s}) {tuple_string}")
+            input(f'Ошибка, ознакомьтесь! Программа продолжит работу после нажатия Enter. \n')
+        else:
+            # если запрос без ошибок - заносим в БД
+            conn2.commit()
+        finally:
+            cur.close()
+            conn2.close()
+
+    def delete_vacancies_by_id(self, id_list: list[str]):
+        """
+        Определенные вакансии позволяет убрать из БД
+        """
+        # Это как 'вычислить по IP' только 'удалить по ID' :)
+        if len(id_list) == 0:
+            print(f"Список ID пуст: {id_list}.")
+            return
+
+        conn2 = psycopg2.connect(**self.conn_params)
+        cur = conn2.cursor()
+
+        try:
+            cur.execute(f"DELETE FROM vacancies WHERE vacancy_id IN ({', '.join(id_list)})")
+        except Exception as e:
+            print(f'\n ОШИБКА: {e} при удалении следующего:')
+            print(f"DELETE FROM vacancies WHERE vacancy_id IN ({', '.join(id_list)})")
+            input(f'Ошибка, ознакомьтесь! Программа продолжит работу после нажатия Enter. \n')
+        else:
+            # если запрос без ошибок - заносим в БД
+            conn2.commit()
+        finally:
+            cur.close()
+            conn2.close()
+
+    def delete_vacancies_with_zero_salary(self):
+        """
+        Вакансии с нулевой зарплатой портят статистику. Удалим их из БД
+        """
+        conn2 = psycopg2.connect(**self.conn_params)
+        cur = conn2.cursor()
+
+        try:
+            cur.execute(f"DELETE FROM vacancies WHERE salary == 0")
+        except Exception as e:
+            print(f'\n ОШИБКА: {e} при удалении следующего:')
+            print(f"DELETE FROM vacancies WHERE salary == 0")
             input(f'Ошибка, ознакомьтесь! Программа продолжит работу после нажатия Enter. \n')
         else:
             # если запрос без ошибок - заносим в БД
@@ -285,14 +331,10 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
                 rows = cur.fetchall()
 
                 new_table = [row for row in rows]
-                # for row in rows:
-                #     newtable.append(row)
 
         conn.close()
         # [print(line) for line in new_table]
-        # input("SEE///")
         return new_table
-
 
     def read_from_file(self) -> list[Vacancy]:
         """
@@ -328,21 +370,100 @@ class DBManager(AbsoluteFileConnector, LoadWrite):
                 cur.execute(f"SELECT * FROM {table_name}")
                 rows = cur.fetchall()
 
-                newtable = []
+                checked_table = []
                 for line in table:
                     for row in rows:
                         if line[0] == row[0]:
                             print(f"совпадение с БД!: {row} == {line}")
                             break
                     else:
-                        newtable.append(line)
+                        checked_table.append(line)
 
         conn.close()
-        # [print(line) for line in newtable]
-        # input("SEE///")
-        return newtable
+        return checked_table
 
+    def get_data_from_bd(self, request, request_name):
+        """
+        Получает данные с помощью запроса
+        Потом это выводится на печать
+        request_name - что запрос должен показать
+        """
+        print(f"ЗАПРОС: {request_name}:")
+        with psycopg2.connect(**self.conn_params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(request)
+                rows = cur.fetchall()
 
+                checked_table = []
+                for row in rows:
+                    print(row)
+
+        conn.close()
+
+    def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий у каждой компании.
+        """
+        request_name = self.get_companies_and_vacancies_count.__doc__
+        request = "SELECT * from employers JOIN (SELECT employer_id, COUNT(*) AS \"vacancies\" \
+        FROM vacancies JOIN employers USING(employer_id) GROUP BY employer_id) AS vacancy_counter \
+        USING (employer_id)  ORDER BY vacancy_counter.vacancies DESC;"
+
+        self.get_data_from_bd(request, request_name)
+
+    def get_all_vacancies(self):
+        """
+        Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
+        """
+        request_name = self.get_all_vacancies.__doc__
+        request = "SELECT vacancies.name AS vacancy, employers.name AS employer, salary, vacancies.url,\
+                    region_name, requirements FROM vacancies \
+                    JOIN regions USING (region_id) \
+                    JOIN employers USING (employer_id) \
+                    ORDER BY vacancies.salary DESC;"
+        self.get_data_from_bd(request, request_name)
+
+    def get_avg_salary(self):
+        """
+        Получает среднюю зарплату по вакансиям.
+        """
+        # По одинаковым? По всем сразу? По вакансиям одной компании?
+        # Ладно, по названию вакансии сгруппирую.
+        # Добавил подсчет количества одноименных вакансий, а то непонятно, как эти данные оценивать
+        request_name = self.get_avg_salary.__doc__
+        request = "SELECT vacancies.name, AVG(vacancies.salary), COUNT(vacancies.name) AS number_of_vacancies \
+                FROM vacancies GROUP BY vacancies.name \
+                ORDER BY AVG(vacancies.salary) DESC;"
+        self.get_data_from_bd(request, request_name)
+
+    def get_vacancies_with_higher_salary(self):
+        """
+        Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
+        """
+        # Только уж тогда средней для вакансий, где зарплата указана
+        request_name = self.get_vacancies_with_higher_salary.__doc__
+        request = "SELECT * FROM vacancies, \
+                (SELECT AVG(vacancies.salary) AS middle_salary FROM vacancies WHERE vacancies.salary > 0) \
+                AS vacancy_avg WHERE vacancies.salary > vacancy_avg.middle_salary \
+                ORDER BY vacancies.salary DESC;"
+        self.get_data_from_bd(request, request_name)
+
+    def get_vacancies_with_keyword(self, word_list: list[str]):
+        """
+        Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
+        """
+        request_name = self.get_vacancies_with_keyword.__doc__
+        head = "SELECT * FROM vacancies"
+        tail = ''
+        for i, w in enumerate(word_list, start=0):
+            if i == 0:
+                tail += f" WHERE vacancies.name LIKE '%{w}%'"
+            else:
+                tail += f" OR vacancies.name LIKE '%{w}%'"
+        else:
+            tail += ' ORDER BY vacancies.name DESC;'
+        request = head + tail
+        self.get_data_from_bd(request, request_name)
 
 
 if __name__ == '__main__':
